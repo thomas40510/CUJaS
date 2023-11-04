6f57d043835aafe37d9dce6bb9c61bbc6db0e036
Thomas Prévost <thomasprevost85@gmail.com>
2023-11-04 22:25:00 +0100

    mv XKey out of Semantics

---
+++ src/main/java/com/apogee/dev/CUJaS/CUJaS_Core/XKey.java
@@ -0,0 +1,17 @@
+package com.apogee.dev.CUJaS.CUJaS_Core;
+
+public enum XKey {
+    BODY,
+    FIGURE,
+    FIG_TYPE,
+    FIG_NAME,
+    FIG_HORIZ,
+    FIG_VERT,
+    FIG_POINTS,
+    FIG_POINT,
+    PT_LAT,
+    PT_LON,
+    BULLSEYE,
+    BULLS_RINGS,
+    BULLS_DIST,
+}
