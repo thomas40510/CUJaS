576df62c95d8ccef1f076d5731d337a884dbf6f1
Thomas Prévost <thomasprevost85@gmail.com>
2023-11-16 13:10:47 +0100

    mv output to /out dir

--- src/main/java/com/apogee/dev/CUJaS/Main.java
+++ src/main/java/com/apogee/dev/CUJaS/Main.java
@@ -1,12 +1,9 @@
 package com.apogee.dev.CUJaS;
 
+import com.apogee.dev.CUJaS.CUJaS_Core.KMLExporter;
 import com.apogee.dev.CUJaS.CUJaS_Core.NTKSemantics;
 import com.apogee.dev.CUJaS.CUJaS_Core.XMLParser;
 import com.apogee.dev.CUJaS.SITACObjects.Figure;
-import com.apogee.dev.CUJaS.CUJaS_Core.KMLExporter;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 
@@ -20,7 +17,7 @@ public class Main {
         parser.parse_sitac();
         ArrayList<Figure> figures = parser.getFigures();
 
-        String kml_path = current_path + "/src/main/resources/output.kml";
+        String kml_path = current_path + "/src/main/out/output.kml";
         KMLExporter exporter = new KMLExporter(figures, kml_path);
         exporter.export();
     }
