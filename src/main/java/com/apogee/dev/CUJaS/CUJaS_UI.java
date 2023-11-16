492de5581142508c03a0d568af6a968bd4ce1c48
Thomas Prévost <thomasprevost85@gmail.com>
2023-11-16 20:03:11 +0100

    👨‍🎨(UI /WIP) // Sélection de fichier depuis l'UI

--- src/main/java/com/apogee/dev/CUJaS/CUJaS_UI.java
+++ src/main/java/com/apogee/dev/CUJaS/CUJaS_UI.java
@@ -1,19 +1,65 @@
 package com.apogee.dev.CUJaS;
 
+import com.apogee.dev.CUJaS.CUJaS_Core.NTKSemantics;
+import com.apogee.dev.CUJaS.CUJaS_Core.XMLParser;
+import com.apogee.dev.CUJaS.SITACObjects.Figure;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class CUJaS_UI {
     private final Logger logger = LogManager.getLogger(CUJaS_UI.class);
 
     private JPanel rootPanel;
-    private JLabel mainLabel;
+    private JButton bFileSelect;
 
     public CUJaS_UI() {
         logger.info("UI initialized.");
 
+        bFileSelect.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // open file picker
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Select SITAC XML file");
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                // open on current directory
+                fileChooser.setCurrentDirectory(new java.io.File("."));
+                // set extension
+                fileChooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
+                    public boolean accept(java.io.File f) {
+                        return f.getName().toLowerCase().endsWith(".xml") || f.isDirectory();
+                    }
+
+                    public String getDescription() {
+                        return "SITAC XML files (*.xml)";
+                    }
+                });
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                fileChooser.showOpenDialog(rootPanel);
+
+                // get selected file
+                String filename = fileChooser.getSelectedFile().getAbsolutePath();
+                logger.info("Selected file: " + filename);
+
+                XMLParser parser = new XMLParser(filename, new NTKSemantics());
+                parser.parse_sitac();
+                ArrayList<Figure> figures = parser.getFigures();
+                for (Figure f : figures) {
+                    logger.debug(f);
+                }
+
+            }
+        });
     }
 
     public static void main(String[] args) {
