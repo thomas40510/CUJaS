38ba9c2d143c90dea156928c6815991c1e316cef
Thomas Prévost <thomasprevost85@gmail.com>
2023-11-19 13:37:16 +0100

    👨‍🎨 (UI /WIP) // choix du langage, et du dossier de sortie

--- src/main/java/com/apogee/dev/CUJaS/UI/CUJaS_UI.java
+++ src/main/java/com/apogee/dev/CUJaS/UI/CUJaS_UI.java
@@ -1,23 +1,23 @@
 package com.apogee.dev.CUJaS.UI;
 
+import com.apogee.dev.CUJaS.Core.MelissaSemantics;
 import com.apogee.dev.CUJaS.Core.NTKSemantics;
-import com.apogee.dev.CUJaS.Core.XMLParser;
-import com.apogee.dev.CUJaS.SITACObjects.Figure;
+import com.apogee.dev.CUJaS.Core.Semantics;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 
 public class CUJaS_UI {
     private final Logger logger = LogManager.getLogger(CUJaS_UI.class);
 
+    private String inputFileName;
+
+    private Semantics semantics;
+
     private JPanel rootPanel;
-    private JButton inputSelectBtn;
     private JButton nextBtn;
     private JRadioButton NTKRadioButton;
     private JRadioButton melissaRadioButton;
@@ -27,11 +27,71 @@ public class CUJaS_UI {
     private JButton outSelectBtn;
     private JTabbedPane tabbedPane1;
     private JScrollPane procPane;
+    private JButton inputBtn;
+    private JLabel inputLocTxt;
     private ButtonGroup langGroup;
 
     public CUJaS_UI() {
+        redirectConsole();
+
         logger.info("UI initialized.");
 
+        nextBtn.addActionListener(e -> {
+            logger.info("Next button pressed.");
+            logger.debug("Next button pressed.");
+            logger.error("Next button pressed.");
+            logger.warn("Next button pressed.");
+        });
+
+        inputBtn.addActionListener(e -> {
+            // open file picker
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Select SITAC XML file");
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            // open on current directory
+            fileChooser.setCurrentDirectory(new java.io.File("."));
+            // set extension
+            fileChooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
+                public boolean accept(java.io.File f) {
+                    return f.getName().toLowerCase().endsWith(".xml") || f.isDirectory();
+                }
+
+                public String getDescription() {
+                    return "SITAC XML files (*.xml)";
+                }
+            });
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            fileChooser.showOpenDialog(rootPanel);
+
+            // get selected file
+            inputFileName = fileChooser.getSelectedFile().getAbsolutePath();
+            inputLocTxt.setText(inputFileName);
+        });
+
+        // listener on radio buttons group
+        melissaRadioButton.addActionListener(e -> semantics = new MelissaSemantics());
+        NTKRadioButton.addActionListener(e -> semantics = new NTKSemantics());
+
+        outSelectBtn.addActionListener(e -> {
+            // open directory picker
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Select output directory");
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            // open on current directory
+            fileChooser.setCurrentDirectory(new java.io.File("."));
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            fileChooser.showOpenDialog(rootPanel);
+
+            // get selected directory
+            String outputDir = fileChooser.getSelectedFile().getAbsolutePath();
+            logger.info("Output directory: " + outputDir);
+        });
+    }
+
+    /**
+     * Redirect stdout and stderr to a {@link com.apogee.dev.CUJaS.UI.ColoredTextPane ColoredTextPane}
+     */
+    private void redirectConsole() {
         ColoredTextPane textPane = new ColoredTextPane();
 
         procPane.setViewportView(textPane);
@@ -45,55 +105,6 @@ public class CUJaS_UI {
         });
         System.setOut(printStream);
         System.setErr(printStream);
-
-
-        nextBtn.addActionListener(e -> {
-            logger.info("Next button pressed.");
-            logger.debug("Next button pressed.");
-            logger.error("Next button pressed.");
-            logger.warn("Next button pressed.");
-        });
-
-        inputSelectBtn.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // open file picker
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setDialogTitle("Select SITAC XML file");
-                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                // open on current directory
-                fileChooser.setCurrentDirectory(new java.io.File("."));
-                // set extension
-                fileChooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
-                    public boolean accept(java.io.File f) {
-                        return f.getName().toLowerCase().endsWith(".xml") || f.isDirectory();
-                    }
-
-                    public String getDescription() {
-                        return "SITAC XML files (*.xml)";
-                    }
-                });
-                fileChooser.setAcceptAllFileFilterUsed(false);
-                fileChooser.showOpenDialog(rootPanel);
-
-                // get selected file
-                String filename = fileChooser.getSelectedFile().getAbsolutePath();
-                logger.info("Selected file: " + filename);
-
-                XMLParser parser = new XMLParser(filename, new NTKSemantics());
-                parser.parse_sitac();
-                ArrayList<Figure> figures = parser.getFigures();
-                for (Figure f : figures) {
-                    logger.debug(f);
-                }
-
-            }
-        });
     }
 
     public static void main(String[] args) {
@@ -105,9 +116,5 @@ public class CUJaS_UI {
         // frame.pack();
         frame.setLocationRelativeTo(null); // center window
         frame.setVisible(true);
-    }
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
     }
 }
