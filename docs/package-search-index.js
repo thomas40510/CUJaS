fec369941136945931c92bb0e73d284038e37a18
Thomas Prévost <thomasprevost85@gmail.com>
2023-11-25 10:02:37 +0100

    📜 (docs/Javadoc) // documentation de l'UI, Javadoc associée

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.apogee.dev.CUJaS"},{"l":"com.apogee.dev.CUJaS.Core"},{"l":"com.apogee.dev.CUJaS.SITACObjects"},{"l":"com.apogee.dev.CUJaS.SITACObjects.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.apogee.dev.CUJaS"},{"l":"com.apogee.dev.CUJaS.Core"},{"l":"com.apogee.dev.CUJaS.SITACObjects"},{"l":"com.apogee.dev.CUJaS.SITACObjects.utils"},{"l":"com.apogee.dev.CUJaS.UI"},{"l":"com.apogee.dev.CUJaS.UI.utils"}];updateSearchResults();
