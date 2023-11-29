8630f9f1e11d8c3cf61e5f09ee9f7dfb61ffae49
Thomas Prévost <thomasprevost85@gmail.com>
2023-11-29 12:24:03 +0100

    📜 (doc/Melissa) // documentation module Melissa

--- src/main/java/com/apogee/dev/CUJaS/Core/XMLParser.java
+++ src/main/java/com/apogee/dev/CUJaS/Core/XMLParser.java
@@ -4,10 +4,18 @@ import com.apogee.dev.CUJaS.SITACObjects.Figure;
 
 import java.util.ArrayList;
 
+/**
+ * Interface de Parser XML. Elle définit les méthodes de base pour parser un fichier XML de SITAC.
+ */
 public interface XMLParser {
 
     void parse_figures() throws RuntimeException;
     void build_figures();
+
+    /**
+     * Renvoie la liste des figures extraites du fichier XML.
+     * @return liste des figures
+     */
     ArrayList<Figure> getFigures();
 
 }
